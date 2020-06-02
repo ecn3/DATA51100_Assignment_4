e2b430bf633c1911c83434845765c9e47dffc697
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-06-01 19:11:00 -0500

    inital file

---
+++ ProbEst.py
@@ -0,0 +1,10 @@
+# Christian Nelson
+# 6/02/2020
+# DATA-51100-002, SUMMER 2020
+# PROGRAMMING ASSIGNMENT #4 ProbEst
+
+# Import pandas
+import pandas as pd
+from pandas import DataFrame
+
+# TODO load in 'cars.csv' into DataFrame
